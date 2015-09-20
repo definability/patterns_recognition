4e23fe4068c1cd6711f4509db34643240b1bd519
char-lie <webmaster976@gmail.com>
2015-09-20 16:04:37 +0300

    Removed 'del'

--- 1_letters/generate_noise.py
+++ 1_letters/generate_noise.py
@@ -7,5 +7,4 @@ def generate_noise(width, height, mu=255.0/2, sigma=255.0/2):
     for i in range(width):
         for j in range(height):
             draw.point((i,j), fill=(0, 0, 0, int(gauss(mu, sigma))))
-    del draw
     return noise
