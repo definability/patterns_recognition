c05229d711bbe6c2819b4c81f8d38294ff28e405
char-lie <5962274+char-lie@users.noreply.github.com>
2015-09-20 16:05:02 +0300

    Fixed noise paste

--- 1_letters/draw_message.py
+++ 1_letters/draw_message.py
@@ -2,8 +2,11 @@ from PIL import Image, ImageDraw, ImageFont
 
 def draw_message(message, size):
     font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', size)
-    image = Image.new('RGBA', font.getsize(message), 'white')
+    width, height = font.getsize(message)
+    o_width, o_height = font.getoffset(message)
+
+    image = Image.new('RGBA', (width, height), (0,0,0,0))
     draw = ImageDraw.Draw(image)
-    draw.text((0, 0), message, 'black', font=font)
-    del draw
-    return image
+    draw.text((-o_width, -o_height), message, 'black', font=font)
+
+    return image.crop(image.getbbox())
