e28a743802aa07433dcb0c04e05af80c9eb9827a
char-lie <5962274+char-lie@users.noreply.github.com>
2015-09-27 17:17:33 +0300

    Fixed error: it's better to multiplicate, rather divide; refactored: added penalty function

--- 1_letters/main.py
+++ 1_letters/main.py
@@ -1,15 +1,39 @@
-from sys import argv
+from sys import argv, float_info
+from math import log
 
 from get_characters import get_characters
 from generate_message import generate_message
 from draw_message import draw_message
 from generate_noise import generate_noise
-from Recognizer import Recognizer, get_penalty
+from Recognizer import Recognizer
 
 from PIL import Image, ImageDraw
 
-EPSILON = 1E-6
+EPSILON = float_info.epsilon
 FONT_SIZE = 14
+SIGMA = 255
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def get_penalty(image, pattern, probability):
+    if probability < EPSILON:
+        return float('inf')
+    result = 0
+    for xv, kv in zip(image, pattern):
+        for x, k in zip(xv, kv):
+            result += (x-k)**2
+    return result - log(probability) * 2 * (SIGMA**2)
+
 
 if __name__ == '__main__':
 
@@ -19,7 +43,9 @@ if __name__ == '__main__':
 
     if [p for p in probabilities if p < 0]:
         raise ValueError('Probabilities can not be negative')
-    elif abs(1 - sum(probabilities)) > EPSILON and ' ' in letters:
+    elif 1 - sum(probabilities) > EPSILON and ' ' in letters:
+        raise ValueError('Sum of probabilities should be equal to 1')
+    elif sum(probabilities) - 1 > EPSILON:
         raise ValueError('Sum of probabilities should be equal to 1')
     elif ' ' not in letters:
         letters.append(' ')
@@ -29,17 +55,22 @@ if __name__ == '__main__':
     message = generate_message(letters, probabilities, count)
     text = draw_message(message, characters)
     width, height = text.size[0], text.size[1]
-    noise = generate_noise(width, height, 0, 555)
+    noise = generate_noise(width, height, 0, SIGMA)
 
     text.paste(Image.new('RGB', (width, height), 'black'), mask=noise)
     image = Image.new('RGB', (width, height), 'white')
     image.paste(Image.new('LA', (width, height), 'black'), mask=text)
 
-    recognizer = Recognizer(characters, image)
-    domains, result = recognizer.calculate(lambda x, y: min(x, y), lambda x, y: x+y, float('inf'), 0)
+    recognizer = Recognizer(image, characters, dict(zip(letters, probabilities)))
+
+    domains, result = recognizer.calculate(lambda x, y: min(x, y), lambda x, y: x+y, float('inf'), 0, get_penalty)
     recognized = recognizer.find_path(domains)
-    print ''.join(recognized)
+
+    #print 'ORIGINAL'
     print ''.join(message)
+    #print 'Simple'
+    print ''.join([('' if m == r else bcolors.FAIL) + r + bcolors.ENDC for m, r in zip(message, recognized)])
     print 1.0*sum([1 for r, m in zip(recognized, message) if r != m])/count
 
     image.save('out.png')
+
