8a05287c188ad61dc21494f9359cd38b1eb29f4f
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-04 17:27:37 +0300

    Added class for Semiring

---
+++ 1_letters/Semiring.py
@@ -0,0 +1,16 @@
+class SemiringElement:
+
+    def __init__(self, value):
+        self.value = value
+
+    def mul(self, element):
+        raise NotImplementedError()
+
+    def add(self, element):
+        raise NotImplementedError()
+
+    def get_zero(self):
+        raise NotImplementedError()
+
+    def get_unity(self):
+        raise NotImplementedError()
