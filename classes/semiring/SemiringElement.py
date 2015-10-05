5d66b4e9ebd80ea9542c8ba905ada511bb4e3fb3
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-05 21:43:02 +0300

    Added abstract Semiring Element class and general tests for Semiring Elements

---
+++ classes/semiring/SemiringElement.py
@@ -0,0 +1,56 @@
+class SemiringElement(object):
+
+
+    def __init__(self, value=None):
+        if value is None:
+            self.value = self.zero()
+        else:
+            self.value = value
+
+
+    def mul(self, element):
+        raise NotImplementedError()
+
+
+    def add(self, element):
+        raise NotImplementedError()
+
+    @classmethod
+    def zero(cls):
+        return cls(cls.get_zero())
+
+    @classmethod
+    def unity(cls):
+        return cls(cls.get_unity())
+
+    @staticmethod
+    def get_zero():
+        raise NotImplementedError()
+
+
+    @staticmethod
+    def get_unity():
+        raise NotImplementedError()
+
+
+    def __iadd__(self, b):
+        return self.add(b)
+
+    def __imul__(self, b):
+        return self.mul(b)
+
+    def __add__(self, b):
+        return self.__class__(self.value).add(b)
+
+    def __mul__(self, b):
+        return self.__class__(self.value).mul(b)
+
+    def __eq__(self, b):
+        return self.value == b.value
+
+    def __neq__(self, b):
+        return not self == b
+
+    def __str__(self):
+        return str(self.value)
+
---
+++ tests/classes/semiring/SemiringMinPlusElement.py
@@ -0,0 +1,44 @@
+from unittest import TestCase, main
+
+from classes.semiring import semirings
+
+
+class TestSemiringsBasicProperties(TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_zero(self):
+        [S.zero() for S in semirings]
+
+
+    def test_unity(self):
+        [S.unity() for S in semirings]
+
+    def test_eq(self):
+        for S in semirings:
+            self.assertEqual(S.zero(), S.zero())
+            self.assertIsNot(S.zero(), S.zero())
+            self.assertEqual(S.unity(), S.unity())
+            self.assertIsNot(S.unity(), S.unity())
+
+    def test_zero_mul_unity(self):
+        for S in semirings:
+            self.assertEqual(S.zero() * S.unity(), S.zero())
+            self.assertEqual(S.unity() * S.zero(), S.zero())
+
+    def test_zero_add_unity(self):
+        for S in semirings:
+            self.assertEqual(S.zero() + S.unity(), S.unity())
+            self.assertEqual(S.unity() + S.zero(), S.unity())
+
+
+if __name__ == '__main__':
+    main()
+
