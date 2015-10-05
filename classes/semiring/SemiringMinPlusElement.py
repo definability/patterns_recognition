2b4d3e82de8fa9c8325c21329ccee92ad59b016f
char-lie <webmaster976@gmail.com>
2015-10-05 21:44:08 +0300

    Added Min-Plus Semiring Element class

---
+++ classes/semiring/SemiringMinPlusElement.py
@@ -0,0 +1,29 @@
+from SemiringElement import SemiringElement
+
+
+class SemiringMinPlusElement(SemiringElement):
+
+
+    def __init__(self, value=None):
+        super(SemiringMinPlusElement, self).__init__(value)
+
+
+    def mul(self, element):
+        self.value += element.value
+        return self
+
+
+    def add(self, element):
+        self.value = min(self.value, element.value)
+        return self
+
+
+    @staticmethod
+    def get_zero():
+        return float('inf')
+
+
+    @staticmethod
+    def get_unity():
+        return 0
+
