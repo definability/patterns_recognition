b2fbd6bada4d008bc9c03ebcd76949b67e2135cd
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-11 18:44:52 +0300

    Initiated Dynamic Programming Problems Solver

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,4 @@
 python -m unittest tests.classes.semiring.SemiringBasicProperties
 python -m unittest tests.classes.graph.GraphBasicProperties
+python -m unittest tests.classes.solver.DynamicProgrammingBasicProperties
+
