611236a3cc4b059fa1157a7d8066e0f77f3b5f06
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-11 17:42:39 +0300

    Created tests for Graph: positive and negative construction

--- tests/classes/graph/GraphBasicProperties.py
+++ tests/classes/graph/GraphBasicProperties.py
@@ -1,6 +1,6 @@
 from unittest import TestCase, main
 
-from classes.graph import Graph
+from classes.graph import Graph, Edge, Vertex
 
 
 class TestGraphBasicProperties(TestCase):
@@ -16,6 +16,23 @@ class TestGraphBasicProperties(TestCase):
 
     def test_constructor(self):
         self.assertIsInstance(Graph(set(), set()), Graph)
+
+
+    def test_one_edge(self):
+        v_zero = Vertex(0)
+        v_one = Vertex(1)
+        e = Edge(v_zero, v_one)
+        self.assertIs(e.get_value(), None)
+        self.assertIsInstance(Graph([v_zero, v_one], [e]), Graph)
+
+
+    def test_incorrect_edge(self):
+        v_zero = Vertex(0)
+        v_one = Vertex(0)
+        self.assertIsNot(v_zero, v_one)
+        e = Edge(v_zero, v_one)
+        with self.assertRaises(ValueError):
+            Graph(v_zero, e)
 
 
 if __name__ == '__main__':
