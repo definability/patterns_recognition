7863f6c58e639160d393123b8215f9350846a34d
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-11 15:54:38 +0300

    Changed Graph: only sets of vertices and edges are available

--- classes/graph/Graph.py
+++ classes/graph/Graph.py
@@ -1,13 +1,23 @@
 class Graph(object):
 
 
-    def __init__(self, V=None, E=None):
-        if V is None:
-            self.V = set()
+    def __init__(self, V, E):
+
+        if not isinstance(V, set):
+            raise ValueError("You should provide set of vertices")
         else:
             self.V = V
-        if E is None:
-            self.E = set()
+
+        if not isinstance(E, set):
+            raise ValueError("You should provide set of edges")
         else:
             self.E = E
+
+        for e in E:
+            self.check_edge(e)
+
+
+    def check_edge(self, e):
+        if e.vertices[0] not in self.V or e.vertices[1] not in self.V:
+            raise ValueError("All vertices should be in graph's vertices' set")
 
