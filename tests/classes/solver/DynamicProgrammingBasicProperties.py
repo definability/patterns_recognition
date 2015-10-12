71eb1f1386ae023b1737463bd9fc2db7136ef551
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-12 22:08:02 +0300

    Corrected tests and fixed errors in Dynamic Programming Problems Solver

--- tests/classes/solver/DynamicProgrammingBasicProperties.py
+++ tests/classes/solver/DynamicProgrammingBasicProperties.py
@@ -3,6 +3,7 @@ from unittest import TestCase, main
 from classes.graph import Graph, Edge, Vertex
 from classes.solver import DynamicProgramming
 from classes.semiring import semirings#SemiringMinPlusElement, SemiringPlusMulElement
+from classes.semiring import SemiringMinPlusElement, SemiringPlusMulElement
 
 
 class TestGraphBasicProperties(TestCase):
@@ -32,43 +33,61 @@ class TestGraphBasicProperties(TestCase):
             g.set_start(vertex)
 
 
-    def test_solve_incorrect_end(self):
+    def test_solve_incorrect_finish(self):
         vertex = Vertex()
         g = DynamicProgramming([], [])
         with self.assertRaises(ValueError):
-            g.set_end(vertex)
+            g.set_finish(vertex)
+
         
-
     def test_solve_simple(self):
-        v_zero = Vertex()
-        v_one = Vertex()
+        """Solve simple problem
+
+        start --1--> finish
+
+        Right answer is 1
+        """
+        start = Vertex('start')
+        finish = Vertex('finish')
         for S in semirings:
-            e = Edge(v_zero, v_one, S.unity())
-            g = DynamicProgramming([v_zero, v_one], [e])
-            g.set_start(v_zero)
-            g.set_end(v_one)
-            self.assertEqual(g.solve(S), S.zero())
+            e = Edge(start, finish, S.unity())
+            g = DynamicProgramming([start, finish], [e])
+            g.set_start(start)
+            g.set_finish(finish)
+            self.assertEqual(g.solve(S), S.unity())
 
 
-    def test_solve_rhombus(self):
+    def test_solve_square(self):
+        """Solve problem with rhombus
 
-        start = Vertex()
-        end = Vertex()
-        a = Vertex()
-        b = Vertex()
+        start ---1--> A
+          |           |
+          1           1
+          |           |
+          V           V
+          B ---1--> finish
+
+        Right answer is (1*1 + 1*1)
+        """
+
+        start = Vertex('start')
+        finish = Vertex('finish')
+        a = Vertex('a')
+        b = Vertex('b')
 
         for S in semirings:
             start_a = Edge(start, a, S.unity())
             start_b = Edge(start, b, S.unity())
 
-            a_end = Edge(a, end, S.unity())
-            b_end = Edge(b, end, S.unity())
+            a_finish = Edge(a, finish, S.unity())
+            b_finish = Edge(b, finish, S.unity())
 
-            g = DynamicProgramming([start, a, b, end], [start_a, start_b, a_end, b_end])
+            g = DynamicProgramming([start, a, b, finish], [start_a, start_b,
+                                                        a_finish, b_finish])
             g.set_start(start)
-            g.set_end(end)
+            g.set_finish(finish)
 
-            self.assertEqual(g.solve(S), S.zero())
+            self.assertEqual(g.solve(S), S.unity()+S.unity())
 
 
 if __name__ == '__main__':
