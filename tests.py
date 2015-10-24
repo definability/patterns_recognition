be5c68a3663ea98519bcd176710438b187f8b0c1
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-24 23:31:27 +0300

    Added profiler to tests

--- tests.py
+++ tests.py
@@ -1,7 +1,14 @@
+import cProfile, pstats, StringIO
 from unittest import TestSuite, TextTestRunner
 from tests import tests
 
+pr = cProfile.Profile()
 
 if __name__ == '__main__':
+    pr.enable()
     TextTestRunner().run(TestSuite(tests))
+    pr.disable()
+    s = StringIO.StringIO()
+    pstats.Stats(pr, stream=s).sort_stats('cumulative').print_stats()
+    print s.getvalue()
 
