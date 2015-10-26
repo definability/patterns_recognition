e8a3fa21b18e53c78c0e512633b78ac92818b630
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-26 21:03:00 +0200

    Refactored tests.py

--- tests.py
+++ tests.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     profile = None
 
     parser = ArgumentParser(description='Testing arguments')
-    parser.add_argument('-p', '--profile',
-            help='Gather and show profiler information', action='store_true')
+    parser.add_argument('-p', '--profile', action='store_true',
+                        help='Gather and show profiler information')
     args = parser.parse_args()
 
     if args.profile:
