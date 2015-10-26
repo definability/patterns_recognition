3a4937bf3e5af48ee179a53db39d9f074cad4a35
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-26 20:44:59 +0200

    Refactored tests.py

--- tests.py
+++ tests.py
@@ -1,25 +1,24 @@
-import argparse
-import cProfile, pstats, StringIO
+from argparse import ArgumentParser
+from cProfile import Profile
+from pstats import Stats
 from unittest import TestSuite, TextTestRunner
 
 from tests import tests
 
-pr = cProfile.Profile()
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Testing arguments')
+    parser = ArgumentParser(description='Testing arguments')
     parser.add_argument('-p', '--profile', help='Gather and show profiler information', action='store_true')
     args = parser.parse_args()
 
     if args.profile:
+        pr = Profile()
         pr.enable()
 
     TextTestRunner().run(TestSuite(tests))
 
     if args.profile:
         pr.disable()
-        s = StringIO.StringIO()
-        pstats.Stats(pr, stream=s).sort_stats('cumulative').print_stats()
-        print s.getvalue()
+        Stats(pr).sort_stats('cumulative').print_stats()
 
