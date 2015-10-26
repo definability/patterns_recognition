791feda9ca9d79446a98e71752b9f254823ab2af
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-26 20:37:51 +0200

    Added 'argparse' to 'tests.py' and arguments for profiler control

--- tests.py
+++ tests.py
@@ -1,14 +1,25 @@
+import argparse
 import cProfile, pstats, StringIO
 from unittest import TestSuite, TextTestRunner
+
 from tests import tests
 
 pr = cProfile.Profile()
 
 if __name__ == '__main__':
-    pr.enable()
+
+    parser = argparse.ArgumentParser(description='Testing arguments')
+    parser.add_argument('-p', '--profile', help='Gather and show profiler information', action='store_true')
+    args = parser.parse_args()
+
+    if args.profile:
+        pr.enable()
+
     TextTestRunner().run(TestSuite(tests))
-    pr.disable()
-    s = StringIO.StringIO()
-    pstats.Stats(pr, stream=s).sort_stats('cumulative').print_stats()
-    print s.getvalue()
+
+    if args.profile:
+        pr.disable()
+        s = StringIO.StringIO()
+        pstats.Stats(pr, stream=s).sort_stats('cumulative').print_stats()
+        print s.getvalue()
 
