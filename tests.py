fdb04f21bde422d288b17ab5146b34702ffc509e
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-26 20:56:08 +0200

    Refactored 'tests.py'

--- tests.py
+++ tests.py
@@ -8,17 +8,19 @@ from tests import tests
 
 if __name__ == '__main__':
 
+    profile = None
+
     parser = ArgumentParser(description='Testing arguments')
     parser.add_argument('-p', '--profile', help='Gather and show profiler information', action='store_true')
     args = parser.parse_args()
 
     if args.profile:
-        pr = Profile()
-        pr.enable()
+        profile = Profile()
+        profile.enable()
 
     TextTestRunner().run(TestSuite(tests))
 
     if args.profile:
-        pr.disable()
-        Stats(pr).sort_stats('cumulative').print_stats()
+        profile.disable()
+        Stats(profile).sort_stats('cumulative').print_stats()
 
