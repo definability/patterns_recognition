52215df25c27ebc3a4ed66841ac69a48ca6d20c7
char-lie <5962274+char-lie@users.noreply.github.com>
2015-11-05 09:20:13 +0200

    Added semiring for argmin-plus

--- classes/semiring/__init__.py
+++ classes/semiring/__init__.py
@@ -1,9 +1,11 @@
 from SemiringMinPlusElement import SemiringMinPlusElement
 from SemiringMinPlusTuple import SemiringMinPlusTuple
 from SemiringPlusMulElement import SemiringPlusMulElement
+from SemiringArgminPlusElement import SemiringArgminPlusElement
 
-semirings = [SemiringMinPlusElement, SemiringMinPlusTuple,SemiringPlusMulElement]
+semirings = [SemiringMinPlusElement, SemiringMinPlusTuple,
+             SemiringPlusMulElement, SemiringArgminPlusElement]
 
 __all__ = ['SemiringMinPlusElement', 'SemiringMinPlusTuple',
-           'SemiringPlusMulElement']
+           'SemiringPlusMulElement', 'SemiringArgminPlusElement']
 
