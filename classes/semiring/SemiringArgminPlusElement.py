a124fc6ab96507d28fbd634347bd4ed9772c1e05
char-lie <webmaster976@gmail.com>
2015-11-05 09:20:13 +0200

    Added semiring for argmin-plus

---
+++ classes/semiring/SemiringArgminPlusElement.py
@@ -0,0 +1,36 @@
+from copy import copy
+
+from SemiringElement import SemiringElement
+
+
+class SemiringArgminPlusElement(SemiringElement):
+
+
+    def __init__(self, value=None):
+        if value is not None and type(value[1]) is not list:
+            value = (value[0], [value])
+        super(SemiringArgminPlusElement, self).__init__(value)
+
+
+    def mul(self, element):
+        self.value = (self.value[0] + element.value[0],
+                      self.value[1] + element.value[1])
+        return self
+
+
+    def add(self, element):
+        if self.value[0] > element.value[0]:
+            self.value = (element.value[0], element.value[1])
+        return self
+
+
+    @staticmethod
+    def get_zero():
+        return (float('inf'), [])
+
+
+    @staticmethod
+    def get_unity():
+        return (0, [])
+
+
