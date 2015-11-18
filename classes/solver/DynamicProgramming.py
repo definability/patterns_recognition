b329633874f09a0f5ca100d1ff70e533f299be9b
char-lie <webmaster976@gmail.com>
2015-11-18 19:03:05 +0200

    Not storing solution info in vertices -- created temporary variable for this purpose

--- classes/solver/DynamicProgramming.py
+++ classes/solver/DynamicProgramming.py
@@ -25,7 +25,6 @@ class DynamicProgramming(Graph):
         for vertex in self.V:
             vertex.clear_inputs()
             vertex.clear_outputs()
-            vertex.set_value(semiring.zero())
 
         self.start.set_value(semiring.unity())
 
@@ -37,11 +36,16 @@ class DynamicProgramming(Graph):
                 edge.set_value(semiring(edge.get_value()))
 
 
-    def __process_vertex(self, vertex, visited, to_visit):
+    def __process_vertex(self, vertex, visited, to_visit, vertices_values):
         for edge in vertex.get_outputs():
 
             finish = edge.get_vertices()[1]
-            finish.set_value(vertex.get_value() * edge.get_value() + finish.get_value())
+            if finish not in vertices_values:
+                vertices_values[finish] = finish.get_value()
+            if vertices_values[finish] is None:
+                vertices_values[finish] = vertices_values[vertex] * edge.get_value()
+            else:
+                vertices_values[finish] += vertices_values[vertex] * edge.get_value()
 
             finish.remove_input(edge)
 
@@ -56,8 +60,10 @@ class DynamicProgramming(Graph):
         to_visit = set([self.finish, self.start])
         visited = set()
 
-        while len(to_visit) and self.finish not in visited:
-            self.__process_vertex(to_visit.pop(), visited, to_visit)
+        vertices_values = {self.start: self.start.get_value()}
 
-        return self.finish.get_value()
+        while len(to_visit) and self.finish not in visited:
+            self.__process_vertex(to_visit.pop(), visited, to_visit, vertices_values)
+
+        return vertices_values[self.finish]
 
