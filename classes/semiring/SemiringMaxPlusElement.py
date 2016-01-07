0c10634d1f76593002ccafa9462a687e8db24b4a
char-lie <webmaster976@gmail.com>
2016-01-07 21:20:48 +0200

    Added (max,+) semiring class

---
+++ classes/semiring/SemiringMaxPlusElement.py
@@ -0,0 +1,29 @@
+from SemiringElement import SemiringElement
+
+
+class SemiringMaxPlusElement(SemiringElement):
+
+
+    def __init__(self, value=None):
+        super(SemiringMaxPlusElement, self).__init__(value)
+
+
+    def mul(self, element):
+        self.value += element.value
+        return self
+
+
+    def add(self, element):
+        self.value = max(self.value, element.value)
+        return self
+
+
+    @staticmethod
+    def get_zero():
+        return float('-inf')
+
+
+    @staticmethod
+    def get_unity():
+        return 0
+
