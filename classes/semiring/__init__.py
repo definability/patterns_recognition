048329af4dfb0b68662f78ad0de87c5b40f2ae39
char-lie <5962274+char-lie@users.noreply.github.com>
2016-01-07 21:20:48 +0200

    Added (max,+) semiring class

--- classes/semiring/__init__.py
+++ classes/semiring/__init__.py
@@ -2,10 +2,13 @@ from SemiringMinPlusElement import SemiringMinPlusElement
 from SemiringMinPlusTuple import SemiringMinPlusTuple
 from SemiringPlusMulElement import SemiringPlusMulElement
 from SemiringArgminPlusElement import SemiringArgminPlusElement
+from SemiringMaxPlusElement import SemiringMaxPlusElement
 
 semirings = [SemiringMinPlusElement, SemiringMinPlusTuple,
-             SemiringPlusMulElement, SemiringArgminPlusElement]
+             SemiringPlusMulElement, SemiringArgminPlusElement,
+             SemiringMaxPlusElement]
 
 __all__ = ['SemiringMinPlusElement', 'SemiringMinPlusTuple',
-           'SemiringPlusMulElement', 'SemiringArgminPlusElement']
+           'SemiringPlusMulElement', 'SemiringArgminPlusElement',
+           'SemiringMaxPlusElement']
 
