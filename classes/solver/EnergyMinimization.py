3c63aad257760841f94eb7f1c444a545a6cb8a35
char-lie <webmaster976@gmail.com>
2016-01-17 22:41:21 +0200

    Corrected EM

--- classes/solver/EnergyMinimization.py
+++ classes/solver/EnergyMinimization.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from classes.graph import Graph
 
 
@@ -14,74 +16,77 @@ class EnergyMinimization(Graph):
             yield n/(i+1)
 
 
-    def __iteration(self, phi, gamma):
-        for domain in self.get_domains():
-            max_k = dict()
-            for v in self.get_domain(domain):
-                print 'ITER', len(v.get_outputs()), len(self.E)
-                print v.get_domain(), v.get_value()
-                for e in v.get_outputs():
-                    print e.get_vertices()[0].get_domain(), e.get_vertices()[0].get_value(), e.get_vertices()[1].get_domain(), e.get_vertices()[1].get_value()
-                    target_domain = e.get_vertices()[1].domain
-                    if target_domain not in max_k:
-                        max_k[(domain, target_domain)] = (None, float('-inf'))
-                    if e.get_vertices() not in phi:
-                        phi[e.get_vertices()] = 0
-                    if e.get_vertices()[::-1] not in phi:
-                        phi[e.get_vertices()[::-1]] = 0
-                    tmp = e.get_value() + phi[e.get_vertices()] \
-                                        + phi[e.get_vertices()[::-1]]
-                    if max_k[(domain, target_domain)][1] < tmp:
-                        max_k[(domain, target_domain)] = (e.get_vertices(), tmp)
-            for d in max_k:
-                phi[max_k[d][0]] -= gamma
-                phi[max_k[d][0][::-1]] -= gamma
-        results = []
-        for e in phi:
-            v1, v2 = e
-            results.append('%s->%s, %d %d %f'%(v1.get_domain(), v2.get_domain(),
-                    v1.get_value(), v2.get_value(), phi[e]))
-        print '\n'.join(sorted(results))
-        print
-        print
+    def max_vertex(self, vertices):
+        return max(vertices, key=lambda v: v.get_value())
 
-        for domain in self.get_domains():
-            max_k = (None, float('-inf'))
-            for v in self.get_domain(domain):
-                tmp = v.get_value()
-                for e in v.get_outputs():
-                    if (v, e[1]) not in phi:
-                        phi[(v, e[1])] = 0
-                    else:
-                        tmp -= phi[e.get_vertices()]
-                for e in v.get_inputs():
-                    if e.get_vertices() not in phi:
-                        phi[e.get_vertices()[::-1]] = 0
-                    else:
-                        tmp -= phi[e.get_vertices()]
-                if max_k[1] < tmp:
-                    max_k = (v, tmp)
-            for e in max_k[0].get_outputs():
-                phi[e.get_vertices()] += gamma
+
+    def max_edge(self, edges):
+        return max(edges, key=lambda e: e.get_value())
+
+
+    def __iteration(self, g, gamma):
+        for d in g.get_domains():
+            max_e = g.max_edge(sum([v.get_outputs()
+                                    for v in g.get_domain(domain)))
+            max_e.set_value(max_e.get_value() - 2 * gamma)
+            x, y = max_e.get_vertices()
+            x.set_value(x.get_value() + gamma)
+            y.set_value(y.get_value() + gamma)
+        for d in g.get_domains():
+            max_v = g.max_vertex(g.get_domain(domain))
+            edges = max_v.get_inputs().union(max_v.get_outputs())
+            max_v.set_value(max_v.get_value() - len(edges) * gamma)
+            for e in edges:
+                e.set_value(e.get_value() + gamma)
+
+
+    def remove_small(self, treshold):
+        for domain in g.get_domains():
+            vertices = g.get_domain(domain)
+            max_v = g.max_vertex(vertices)
+            for v in vertices:
+                if v.get_value() - max_v.get_value() > treshold:
+                    g.delete_vertex(v)
+        for domain in g.get_domains():
+            edges = sum([v.get_outputs() for v in g.get_domain(domain))
+            max_e = g.max_edge(edges)
+            for e in edges:
+                if e.get_value() - max_e.get_value() > treshold:
+                    g.delete_edge(e)
 
 
     def solve(self):
-        phi = dict()
-        for gamma in self.__gamma():
-            self.__iteration(phi, gamma)
+        V_map = dict()
+        V_map_inv = dict()
+        E_map = dict()
+        for v in self.V:
+            V_map[deepcopy(v)] = v
+            V_map_inv[v] = deepcopy(v)
+        for e in self.E:
+            x, y = e.get_vertices()
+            E_map[Edge(V_map_inv(x), V_map_inv(y), deepcopy(e.get_value())) = e
+        g = Graph(V_map.keys(), E_map.keys())
+        self.prepare()
+        for gamma in self.__gamma(10):
+            g.remove_small(0.5)
+            if g.is_neighborhood_corrupted():
+                break
+            g.restore()
+            self.__iteration(g, gamma)
             break
-        result = dict()
-        for domain in self.get_domains():
-            result[domain] = (None, float('-inf'))
-            for v in self.get_domain(domain):
-                tmp = v.get_value() \
-                        - sum(phi[e.get_vertices()] for e in v.get_outputs()) \
-                        - sum(phi[e.get_vertices()[::-1]] for e in v.get_inputs())
-                print '%s (%d): %f'%(v.get_domain(), v.get_value(), tmp)
-                if result[domain][1] < tmp:
-                    result[domain] = (v, tmp)
-        print
-        ##print phi
-        print [(result[domain][0].get_value(), result[domain][1]) for domain in result]
-        return set(result[domain][0] for domain in result)
+        result = []
+        domain = g.get_domains().pop()
+        vertices.append(g.get_domain(domain).pop())
+        edges = []
+        while True:
+            inputs = result[0].get_inputs()
+            outputs = result[-1].get_outputs()
+            if len(inputs) > 0:
+                edges =  [inputs.pop()] + edges
+                result = edges[0].get_vertices()[1] + result
+            if len(outputs) > 0:
+                edges =  edges + [outputs.pop()]
+                result.append(edges[-1].get_vertices[0])
+        #return (set(V_map[v] for v in vertices), set(E_map[e] for e in edges))
+        return set(V_map[v] for v in vertices)
 
