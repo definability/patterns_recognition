f965d23effce6224449772316fcf12fb8a0684e5
char-lie <5962274+char-lie@users.noreply.github.com>
2016-01-27 00:19:50 +0200

    Updated EM example: added profiling

--- examples/energy_minimization/rectangle_tracker/__main__.py
+++ examples/energy_minimization/rectangle_tracker/__main__.py
@@ -3,6 +3,8 @@ from os import path
 from build_problem import build_problem
 
 from PIL import Image, ImageDraw
+from cProfile import Profile
+from pstats import Stats
 
 
 if __name__ == '__main__':
@@ -10,11 +12,16 @@ if __name__ == '__main__':
     model_image_mask = [p[3] > 0 for p in list(model_image.getdata())]
     model_image_grayscale = model_image.convert('L')
 
-    raw_image = Image.open(path.join(path.dirname(__file__), 'rect_raw.png'))
+    raw_image = Image.open(path.join(path.dirname(__file__), 'rect_red.png'))
     raw_image_grayscale = raw_image.convert('L')
 
     problem = build_problem(model_image_grayscale, raw_image_grayscale,
                             model_image_mask)
+    profile = Profile()
+    profile.enable()
+    solution = problem.solve()
+    profile.disable()
+    Stats(profile).sort_stats('time').print_stats()
     print sorted([(v.get_name(), v.get_value(), v.get_domain())
-                  for v in problem.solve()], key=lambda x: x[0][0] * raw_image.size[0] + x[0][1])
+                  for v in solution], key=lambda x: x[0][0] * raw_image.size[0] + x[0][1])
 
