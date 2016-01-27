aaccf53687ff2313ff68c36857aa982d3395809f
char-lie <webmaster976@gmail.com>
2016-01-28 00:50:08 +0200

    Refactored EM RT example: prettified output

--- examples/energy_minimization/rectangle_tracker/__main__.py
+++ examples/energy_minimization/rectangle_tracker/__main__.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     model_image_mask = [p[3] > 0 for p in list(model_image.getdata())]
     model_image_grayscale = model_image.convert('L')
 
-    raw_image = Image.open(path.join(path.dirname(__file__), 'rect_red.png'))
+    raw_image = Image.open(path.join(path.dirname(__file__), 'rect_raw.png'))
     raw_image_grayscale = raw_image.convert('L')
 
     problem = build_problem(model_image_grayscale, raw_image_grayscale,
@@ -22,6 +22,8 @@ if __name__ == '__main__':
     solution = problem.solve()
     profile.disable()
     Stats(profile).sort_stats('time').print_stats()
-    print sorted([(v.get_name(), v.get_value(), v.get_domain())
+    result = sorted([(v.get_name(), v.get_domain())
                   for v in solution], key=lambda x: x[0][0] * raw_image.size[0] + x[0][1])
+    for r in result:
+        print 'Model', r[1], 'to', r[0]
 
