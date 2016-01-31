a98b42bc9a6226b454b94889ce8be4a200938cf1
char-lie <5962274+char-lie@users.noreply.github.com>
2016-01-31 15:52:51 +0200

    Updated EM RT example: using bigger rectangle and not copying graph

--- examples/energy_minimization/rectangle_tracker/__main__.py
+++ examples/energy_minimization/rectangle_tracker/__main__.py
@@ -8,18 +8,18 @@ from pstats import Stats
 
 
 if __name__ == '__main__':
-    model_image = Image.open(path.join(path.dirname(__file__), 'rect_model.png'))
+    model_image = Image.open(path.join(path.dirname(__file__), 'rect_model_10x10.png'))
     model_image_mask = [p[3] > 0 for p in list(model_image.getdata())]
     model_image_grayscale = model_image.convert('L')
 
-    raw_image = Image.open(path.join(path.dirname(__file__), 'rect_raw.png'))
+    raw_image = Image.open(path.join(path.dirname(__file__), 'rect_big_20x20.png'))
     raw_image_grayscale = raw_image.convert('L')
 
     problem = build_problem(model_image_grayscale, raw_image_grayscale,
                             model_image_mask)
     profile = Profile()
     profile.enable()
-    solution = problem.solve()
+    solution = problem.solve(False)
     profile.disable()
     Stats(profile).sort_stats('time').print_stats()
     result = sorted([(v.get_name(), v.get_domain())
