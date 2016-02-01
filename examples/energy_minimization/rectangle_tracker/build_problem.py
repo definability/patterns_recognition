f15b77d6677a20e4b3bc7ba05c07bd901bfcb14c
char-lie <webmaster976@gmail.com>
2016-02-01 19:48:11 +0200

    Parametrized problem building: added offset

--- examples/energy_minimization/rectangle_tracker/build_problem.py
+++ examples/energy_minimization/rectangle_tracker/build_problem.py
@@ -8,7 +8,8 @@ from cProfile import Profile
 from pstats import Stats
 
 
-def build_problem(model_image, raw_image, model_image_mask=None):
+def build_problem(model_image, raw_image, model_image_mask=None,
+        max_vertical_offset=float('inf'), max_horizontal_offset=float('inf')):
     if model_image_mask is None:
         model_image_mask = [True] * len(model_image.getdata())
     model = MatrixPointer(list(model_image.getdata()),
@@ -19,44 +20,59 @@ def build_problem(model_image, raw_image, model_image_mask=None):
                          (raw_image.size[0], raw_image.size[1]))
     profile = Profile()
     profile.enable()
-    vertices, edges = process_image(model, raw, mask)
+    vertices, edges = process_image(model, raw, mask, max_vertical_offset,
+                                    max_horizontal_offset)
     profile.disable()
     Stats(profile).sort_stats('time').print_stats()
     problem = EnergyMinimization(vertices, edges)
     return problem
 
 
-def process_image(model, raw, mask):
+def process_image(model, raw, mask, max_vertical_offset, max_horizontal_offset):
     vertices = list()
     edges = list()
     domains = dict()
 
+    raw_size = raw.get_size()
     for i in xrange(model.get_size()[0]):
         for j in xrange(model.get_size()[1]):
             if not mask[i,j]:
                 continue
-            domains[(i,j)] = create_vertices((i,j), raw, model)
+            vertical_from = max(0, i - max_vertical_offset)
+            horizontal_from = max(0, j - max_horizontal_offset)
+            vertical_to = min(raw_size[0], i + max_vertical_offset + 1)
+            horizontal_to = min(raw_size[1], j + max_horizontal_offset + 1)
+            domains[(i,j)] = create_vertices((i,j), raw, model,
+                    xrange(vertical_from, vertical_to),
+                    xrange(horizontal_from, horizontal_to))
             vertices += domains[(i,j)].values()
 
     for domain in domains:
         neighbours = get_neighbours(model, domain, mask)
         for pixel in domains[domain]:
             vertex = domains[domain][pixel]
+            vertex_pos = vertex.get_name()
+            vertical_to = min(raw_size[0], vertex_pos[0] + max_vertical_offset + 1)
+            horizontal_to = min(raw_size[1], vertex_pos[1] + max_horizontal_offset + 1)
             for neighbour_domain in neighbours:
-                process_domain(model, raw, neighbour_domain,
-                               vertex, pixel, domains, vertices, edges)
+                process_domain(model, raw, neighbour_domain, vertex, pixel,
+                               domains, vertices, edges,
+                               xrange(pixel[0], vertical_to),
+                               xrange(pixel[1], horizontal_to))
 
     return (set(vertices), set(edges))
 
 
-def process_domain(model, raw, domain, start, pixel, domains, vertices, edges):
+def process_domain(model, raw, domain, start, pixel, domains, vertices, edges,
+        vertical_range, horizontal_range):
     needed_offset = (domain[0] - start.get_domain()[0],
                      domain[1] - start.get_domain()[1])
     start_pos = start.get_name()
-    max_i, max_j = raw.get_size()
-    for i in xrange(pixel[0], max_i):
-        for j in xrange(pixel[1], max_j):
+    for i in vertical_range:
+        for j in horizontal_range:
             end_pos = (i,j)
+            if end_pos not in domains[domain]:
+                continue
             process_end(edges, start, domains[domain][end_pos],
                         start_pos, end_pos, needed_offset)
 
@@ -68,11 +84,11 @@ def process_end(edges, start, end, start_pos, end_pos, needed_offset):
     edges.append(edge)
 
 
-def create_vertices(domain, raw, model):
+def create_vertices(domain, raw, model, vertical_range, horizontal_range):
     return dict(((i,j), Vertex((i,j),
                         get_value_penalty(model[domain], raw[i,j]), domain))
-               for i in xrange(raw.get_size()[0])
-               for j in xrange(raw.get_size()[1]))
+               for i in vertical_range
+               for j in horizontal_range)
 
 
 def get_neighbours(model, pos, mask):
