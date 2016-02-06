6f24fd9d28bc39186a245cfbfd5baba115afa183
char-lie <webmaster976@gmail.com>
2016-02-06 17:04:01 +0200

    Updated EM RT example: setting logging level from command line

--- examples/energy_minimization/rectangle_tracker/__main__.py
+++ examples/energy_minimization/rectangle_tracker/__main__.py
@@ -1,4 +1,6 @@
 from os import path
+import logging
+from argparse import ArgumentParser
 
 from build_problem import build_problem
 
@@ -8,6 +10,14 @@ from pstats import Stats
 
 
 if __name__ == '__main__':
+    parser = ArgumentParser(description='Rectangle tracker example')
+    logger_levels = [logging.getLevelName(i*10) for i in xrange(6)]
+    parser.add_argument('-l', '--log', help='set logger level', type=str.upper,
+                                       default='ERROR', choices=logger_levels)
+    args = parser.parse_args()
+
+    logging.basicConfig(level=getattr(logging, args.log))
+
     model_image = Image.open(path.join(path.dirname(__file__), 'rect_model_10x10.png'))
     model_image_mask = [p[3] > 0 for p in list(model_image.getdata())]
     model_image_grayscale = model_image.convert('L')
