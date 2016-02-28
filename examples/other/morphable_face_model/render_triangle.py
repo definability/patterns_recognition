5d739b9cbcbf36d7be01ac1ee869231cb6a22436
char-lie <webmaster976@gmail.com>
2016-02-28 22:16:42 +0200

    Updated MFM example: moded triangle rendering to C

--- examples/other/morphable_face_model/render_triangle.py
+++ examples/other/morphable_face_model/render_triangle.py
@@ -2,7 +2,6 @@ from numpy import array
 import ctypes
 
 lib = ctypes.cdll.LoadLibrary('./lib_draw_line.so')
-draw_scanline = lib.draw_scanline
 
 def rasterize_triangles(canvas, vertices, z_indices, colors, triangles):
     for triangle in triangles:
@@ -14,21 +13,6 @@ def rasterize_triangles(canvas, vertices, z_indices, colors, triangles):
 
 def prepare_triangle(canvas, vertices, color):
     vertices = vertices[vertices[:,1].argsort()[::-1]]
-    if int(.5+vertices[1][1]) == int(.5+vertices[2][1]):
-        lib.fill_top_flat_triangle(canvas.ctypes.get_as_parameter(),
+    lib.prepare_triangle(canvas.ctypes.get_as_parameter(),
                 vertices.ctypes.get_as_parameter(), int(color), canvas.shape[1])
-    elif int(.5+vertices[0][1]) == int(.5+vertices[1][1]):
-        lib.fill_bottom_flat_triangle(canvas.ctypes.get_as_parameter(),
-                vertices.ctypes.get_as_parameter(), int(color), canvas.shape[1])
-    else:
-        middle = array([vertices[0][0] + ((vertices[1][1] - vertices[0][1])
-                                        / (vertices[2][1] - vertices[0][1]))
-                                        * (vertices[2][0] - vertices[0][0]),
-                        vertices[1][1]])
-        tf = array([vertices[0], vertices[1], middle])
-        bf = array([vertices[1], middle, vertices[2]])
-        lib.fill_bottom_flat_triangle(canvas.ctypes.get_as_parameter(),
-                bf.ctypes.get_as_parameter(), int(color), canvas.shape[1])
-        lib.fill_top_flat_triangle(canvas.ctypes.get_as_parameter(),
-                tf.ctypes.get_as_parameter(), int(color), canvas.shape[1])
 
