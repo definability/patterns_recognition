4cec7eb001c1d2d2b246f7aa26b6b23cb3956eb1
char-lie <webmaster976@gmail.com>
2016-02-28 22:59:51 +0200

    Updated MFM example: totally translated single triangle rendering to C

--- examples/other/morphable_face_model/render_triangle.py
+++ examples/other/morphable_face_model/render_triangle.py
@@ -6,13 +6,9 @@ lib = ctypes.cdll.LoadLibrary('./lib_draw_line.so')
 def rasterize_triangles(canvas, vertices, z_indices, colors, triangles):
     for triangle in triangles:
         current_vertices = vertices[triangle]
-        z_index = z_indices[triangle].mean()
-        color = z_index
-        prepare_triangle(canvas, current_vertices[:,:2], color)
+        color = z_indices[triangle].mean()
+        lib.prepare_triangle(canvas.ctypes.get_as_parameter(),
+                             current_vertices.ctypes.get_as_parameter(),
+                             int(color), canvas.shape[1])
 
-
-def prepare_triangle(canvas, vertices, color):
-    vertices = vertices[vertices[:,1].argsort()[::-1]]
-    lib.prepare_triangle(canvas.ctypes.get_as_parameter(),
-                vertices.ctypes.get_as_parameter(), int(color), canvas.shape[1])
 
