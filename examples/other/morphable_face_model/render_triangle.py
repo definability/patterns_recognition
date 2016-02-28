5274b533fcdafbda76f5df68926dbabc457148a6
char-lie <webmaster976@gmail.com>
2016-02-28 23:55:04 +0200

    Updated MFM: optimized triangle rendering loop speed

--- examples/other/morphable_face_model/render_triangle.py
+++ examples/other/morphable_face_model/render_triangle.py
@@ -4,10 +4,11 @@ import ctypes
 lib = ctypes.cdll.LoadLibrary('./lib_draw_line.so')
 
 def rasterize_triangles(canvas, vertices, z_indices, colors, triangles):
+    canvas_width = canvas.shape[1]
+    canvas = canvas.ctypes.get_as_parameter()
     for triangle in triangles:
         current_vertices = vertices[triangle]
         color = z_indices[triangle].mean()
-        lib.prepare_triangle(canvas.ctypes.get_as_parameter(),
-                             current_vertices.ctypes.get_as_parameter(),
-                             int(color), canvas.shape[1])
+        lib.prepare_triangle(canvas, current_vertices.ctypes.get_as_parameter(),
+                             int(color), canvas_width)
 
