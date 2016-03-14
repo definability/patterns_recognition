ab5a17b38105cb975c3d0c33b46b5f87f3b7ff2a
char-lie <webmaster976@gmail.com>
2016-03-14 23:55:32 +0200

    Updated MFM example
    Using GLFW to render the face

---
+++ examples/other/morphable_face_model/render_face.c
@@ -0,0 +1,106 @@
+#include <GLFW/glfw3.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+float rx = 0.f, ry = 0.f, rz = 0.f, last_rotated = 0.f;
+static void refresh_rotation() {
+    rx = 0.f;
+    ry = 0.f;
+    rz = 0.f;
+}
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    if (key == GLFW_KEY_LEFT && action != GLFW_RELEASE) {
+        refresh_rotation();
+        ry = -1.f;
+        last_rotated += 1.f;
+    }
+    if (key == GLFW_KEY_RIGHT && action != GLFW_RELEASE) {
+        refresh_rotation();
+        ry = 1.f;
+        last_rotated += 1.f;
+    }
+    if (key == GLFW_KEY_DOWN && action != GLFW_RELEASE) {
+        refresh_rotation();
+        rx = 1.f;
+        last_rotated += 1.f;
+    }
+    if (key == GLFW_KEY_UP && action != GLFW_RELEASE) {
+        refresh_rotation();
+        rx = -1.f;
+        last_rotated += 1.f;
+    }
+    if (key == GLFW_KEY_Z && action != GLFW_RELEASE) {
+        refresh_rotation();
+        rz = -1.f;
+        last_rotated += 1.f;
+    }
+    if (key == GLFW_KEY_A && action != GLFW_RELEASE) {
+        refresh_rotation();
+        rz = 1.f;
+        last_rotated += 1.f;
+    }
+    if (action == GLFW_RELEASE) {
+        refresh_rotation();
+        last_rotated = 0.f;
+    }
+}
+void render_face(float* vertices, unsigned short* triangles, int amount) {
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    glfwSetKeyCallback(window, key_callback);
+    refresh_rotation();
+    printf("amount %u\n", amount);
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float) height;
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-2.f, 2.f, -2.f, 2.f, -2.f, 2.f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glRotatef(last_rotated, rx, ry, rz);
+        glBegin(GL_TRIANGLES);
+        size_t i = 0, j;
+        float r, g, b;
+        while (i < amount) {
+            //j = triangles[i];
+            j = i;
+            r = vertices[3*j];
+            g = vertices[3*j + 1];
+            b = vertices[3*j + 2];
+            glColor3f(r, g, b);
+            //glColor3f(1.f, 1.f, 1.f);
+            glVertex3f(vertices[3*j], vertices[3*j+1], vertices[3*j+2]);
+            i++;
+        }
+        glEnd();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
+
--- examples/other/morphable_face_model/render_triangle.py
+++ examples/other/morphable_face_model/render_triangle.py
@@ -1,14 +1,13 @@
-from numpy import array
+from numpy import array, column_stack
 import ctypes
 
-lib = ctypes.cdll.LoadLibrary('./lib_draw_line.so')
+render_face = ctypes.cdll.LoadLibrary('./lib_render_face.so')
 
-def rasterize_triangles(canvas, vertices, z_indices, colors, triangles):
-    canvas_width = canvas.shape[1]
-    canvas = canvas.ctypes.get_as_parameter()
-    for triangle in triangles:
-        current_vertices = vertices[triangle]
-        color = z_indices[triangle].mean()
-        lib.prepare_triangle(canvas, current_vertices.ctypes.get_as_parameter(),
-                             int(color), canvas_width)
+def rasterize_triangles(vertices, triangles):
+    v = vertices[triangles]
+    v /= max(v.max(), -v.min())
+    print len(v), v.size, v.shape, triangles.max()*3+2
+    #amount = triangles.size
+    amount = 3 * len(v)
+    render_face.render_face(v.ctypes.get_as_parameter(), triangles.ctypes.get_as_parameter(), amount)
 
