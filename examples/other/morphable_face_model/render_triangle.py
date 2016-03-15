4c679dd2f92112308eda400f521538649d70ec67
char-lie <5962274+char-lie@users.noreply.github.com>
2016-03-15 22:00:40 +0200

    Updated MFM example
    Forming triangles within C code

--- examples/other/morphable_face_model/render_triangle.py
+++ examples/other/morphable_face_model/render_triangle.py
@@ -4,10 +4,9 @@ import ctypes
 render_face = ctypes.cdll.LoadLibrary('./lib_render_face.so')
 
 def rasterize_triangles(vertices, triangles):
-    v = vertices[triangles]
-    v /= max(v.max(), -v.min())
-    print len(v), v.size, v.shape, triangles.max()*3+2
-    #amount = triangles.size
-    amount = 3 * len(v)
-    render_face.render_face(v.ctypes.get_as_parameter(), triangles.ctypes.get_as_parameter(), amount)
+    vertices = vertices - vertices.min()
+    vertices /= vertices.max()
+    render_face.render_face(vertices.ctypes.get_as_parameter(),
+                            triangles.ctypes.get_as_parameter(),
+                            triangles.size)
 
