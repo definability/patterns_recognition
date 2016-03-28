b17cb61e0ad0d69d64de0bf117e394a82d8afb1f
char-lie <webmaster976@gmail.com>
2016-03-28 22:00:10 +0300

    Updated MFM example
    Added profiler info

--- examples/other/morphable_face_model/triangle_example.py
+++ examples/other/morphable_face_model/triangle_example.py
@@ -4,7 +4,13 @@ from render_triangle import rasterize_triangles
 from PIL import Image
 from numpy import array
 
-from calculations import get_normals, set_light
+from calculations import get_neighbours, get_normals, set_light
+
+from cProfile import Profile
+from pstats import Stats
+
+profile = Profile()
+profile.enable()
 
 model = load_model()
 
@@ -15,5 +21,9 @@ triangles = model['tl'] - 1
 normals = get_normals(points, triangles)
 light_direction = array([-1, 0, -1])/(2**.5)
 lights = set_light(normals, light_direction).astype('f')
+
+profile.disable()
+Stats(profile).sort_stats('tottime').print_stats()
+
 rasterize_triangles(points, triangles.flatten(), lights)
 
