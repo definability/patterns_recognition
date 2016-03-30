5ba0dfd1ba048a13399c003c4cdb0af00acfab67
char-lie <5962274+char-lie@users.noreply.github.com>
2016-03-30 23:54:16 +0300

    Refactored MFM example
    Using arrays with rotation parameters
    
    This allows to process key presses in a loop instead of writing bunch of code to process each key event

--- examples/other/morphable_face_model/render_face.c
+++ examples/other/morphable_face_model/render_face.c
@@ -3,55 +3,34 @@
 #include <stdio.h>
 #include <stdint.h>
 
-float rx = 0.f, ry = 0.f, rz = 0.f, last_rotated = 0.f;
-static void refresh_rotation() {
-    rx = 0.f;
-    ry = 0.f;
-    rz = 0.f;
-}
+float rotation[] = {0.f, 0.f, 0.f};
+int forward_rotation_keys[] = {GLFW_KEY_DOWN, GLFW_KEY_LEFT, GLFW_KEY_Z};
+int back_rotation_keys[] = {GLFW_KEY_UP, GLFW_KEY_RIGHT, GLFW_KEY_A};
+
 static void error_callback(int error, const char* description)
 {
     fputs(description, stderr);
 }
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+
+static void key_callback(GLFWwindow* window, int key, int scancode,
+                                             int action, int mods)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, GL_TRUE);
-    if (key == GLFW_KEY_LEFT && action != GLFW_RELEASE) {
-        refresh_rotation();
-        ry = 1.f;
-        last_rotated += 1.f;
     }
-    if (key == GLFW_KEY_RIGHT && action != GLFW_RELEASE) {
-        refresh_rotation();
-        ry = -1.f;
-        last_rotated += 1.f;
-    }
-    if (key == GLFW_KEY_DOWN && action != GLFW_RELEASE) {
-        refresh_rotation();
-        rx = 1.f;
-        last_rotated += 1.f;
-    }
-    if (key == GLFW_KEY_UP && action != GLFW_RELEASE) {
-        refresh_rotation();
-        rx = -1.f;
-        last_rotated += 1.f;
-    }
-    if (key == GLFW_KEY_Z && action != GLFW_RELEASE) {
-        refresh_rotation();
-        rz = -1.f;
-        last_rotated += 1.f;
-    }
-    if (key == GLFW_KEY_A && action != GLFW_RELEASE) {
-        refresh_rotation();
-        rz = 1.f;
-        last_rotated += 1.f;
-    }
-    if (action == GLFW_RELEASE) {
-        refresh_rotation();
-        last_rotated = 0.f;
-    }
+
+    int i = 0;
+    do {
+        if (key == forward_rotation_keys[i] && action != GLFW_RELEASE) {
+            rotation[i] = 1.f;
+        } else if (key == back_rotation_keys[i] && action != GLFW_RELEASE) {
+            rotation[i] = -1.f;
+        } else if (key == back_rotation_keys[i] || key == forward_rotation_keys[i]) {
+            rotation[i] = 0.f;
+        }
+    } while (++i < sizeof(rotation) / sizeof(*rotation));
 }
+
 void render_face(float* vertices, float* colors, uint16_t* triangles, int amount) {
     GLFWwindow* window;
     glfwSetErrorCallback(error_callback);
@@ -83,7 +62,7 @@ void render_face(float* vertices, float* colors, uint16_t* triangles, int amount
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glClearColor(1.f, 1.f, 1.f, 0.f);
 
-        glRotatef(last_rotated, rx, ry, rz);
+        glRotatef(1.f, rotation[0], rotation[1], rotation[2]);
         glBegin(GL_TRIANGLES);
         size_t i = 0;
         unsigned short j = 0;
