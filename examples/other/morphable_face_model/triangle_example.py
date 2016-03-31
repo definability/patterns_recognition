8a12e22b39e5f5267e67964af972e8f90e340beb
char-lie <webmaster976@gmail.com>
2016-03-31 21:08:48 +0300

    Updated MFM example
    Example controlled by keys within OpenGL application window
    
    Esc: change face model
    
    Q: quit from application
    
    N: toggle normal map and light map as a texture
    
    Generating random face on each iteration

--- examples/other/morphable_face_model/triangle_example.py
+++ examples/other/morphable_face_model/triangle_example.py
@@ -2,30 +2,48 @@ from load_model import *
 from render_triangle import rasterize_triangles
 
 from PIL import Image
-from numpy import array
+from numpy import array, zeros
+from numpy.random import randn
 
 from calculations import get_normals, set_light, get_normal_map, centralize
 
 from cProfile import Profile
 from pstats import Stats
 
-profile = Profile()
-profile.enable()
-
 model = load_model()
 
-coorinates = model['shapeMU']
-points = centralize(coorinates.reshape(coorinates.shape[0]/3, 3))
-triangles = model['tl'] - 1
+redraw = True
+light = True
+while True:
+    profile = Profile()
+    profile.enable()
 
-normals = get_normals(points, triangles)
-light_direction = array([-1, 0, -1])/(2**.5)
-lights = set_light(normals, light_direction).astype('f')
-normal_map = get_normal_map(normals).astype('f')
+    if redraw:
+        lights = None
+        normal_map = None
 
-profile.disable()
-Stats(profile).sort_stats('tottime').print_stats()
+        coordinates = morph(model, randn(199, 1)).astype('f')
+        #coordinates = morph(model, zeros(199).reshape(199, 1)).astype('f')
+        #coordinates = model['shapeMU']
+        points = centralize(coordinates.reshape(coordinates.shape[0]/3, 3))
+        triangles = model['tl'] - 1
+        normals = get_normals(points, triangles)
 
-colors = normal_map
-rasterize_triangles(points, triangles.flatten(), colors)
+    if lights is None and light:
+        light_direction = array([-1, 0, -1])/(2**.5)
+        lights = set_light(normals, light_direction).astype('f')
+    elif normal_map is None and not light:
+        normal_map = get_normal_map(normals).astype('f')
+
+    profile.disable()
+    Stats(profile).sort_stats('tottime').print_stats()
+
+    colors = lights if light else normal_map
+    status = rasterize_triangles(points, triangles.flatten(), colors)
+    redraw = True
+    if status == 0:
+        break
+    elif status == 2:
+        redraw = False
+        light = not light
 
