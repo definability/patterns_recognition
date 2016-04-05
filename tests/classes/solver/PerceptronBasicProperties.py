45ffeb670df8500c8519d8cc4047685857e36a8e
char-lie <5962274+char-lie@users.noreply.github.com>
2016-04-05 20:43:52 +0300

    Updated PS unit tests
    Checking classification quality

--- tests/classes/solver/PerceptronBasicProperties.py
+++ tests/classes/solver/PerceptronBasicProperties.py
@@ -43,6 +43,36 @@ class TestPerceptronBasicProperties(TestCase):
             perceptron.setup(process(left), process(right))
 
 
+    def test_classify_line_zero(self):
+        left = [[-1]]
+        right = [[1]]
+
+        left_test = [[-5], [-10]]
+        right_test = [[5], [10]]
+        for process in self.processors:
+            perceptron = Perceptron(1)
+            perceptron.setup(process(left), process(right))
+            for l in process(left_test):
+                self.assertEqual(perceptron.classify_vertex(l), -1)
+            for r in process(right_test):
+                self.assertEqual(perceptron.classify_vertex(r), 1)
+
+
+    def test_classify_line_offset(self):
+        left = [[-3, 1], [-2, 1]]
+        right = [[-1, 1], [10, 1]]
+
+        left_test = [[-3, 1], [-20, 1]]
+        right_test = [[-1, 1], [0, 1]]
+        for process in self.processors:
+            perceptron = Perceptron(2)
+            perceptron.setup(process(left), process(right))
+            for l in process(left_test):
+                self.assertEqual(perceptron.classify_vertex(l), -1)
+            for r in process(right_test):
+                self.assertEqual(perceptron.classify_vertex(r), 1)
+
+
 if __name__ == '__main__':
     main()
 
