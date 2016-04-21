81921d0bb7e1b2e784bbc56d77e17288705c1c50
char-lie <5962274+char-lie@users.noreply.github.com>
2016-04-21 20:20:46 +0300

    Removed MFM example

--- examples/other/morphable_face_model/mayavi.py
+++
@@ -1,7 +0,0 @@
-from mayavi import mlab
-from load_model import get_figure
-from numpy.random import randn
-
-shape, triangles = get_figure(coefficients=randn(199, 1))
-mlab.triangular_mesh(shape[0], shape[1], shape[2], triangles, color=(1,1,1))
-
