02c67acf5a936f3acdd1e445d7eedea0de24c3b7
char-lie <5962274+char-lie@users.noreply.github.com>
2016-04-21 20:08:07 +0300

    Removed Energy Minimization solver from master branch

--- tests/classes/solver/__init__.py
+++ tests/classes/solver/__init__.py
@@ -1,8 +1,6 @@
 from DynamicProgrammingBasicProperties import TestDynamicProgrammingBasicProperties
-from EnergyMinimizationBasicProperties import TestEnergyMinimizationBasicProperties
 from PerceptronBasicProperties import TestPerceptronBasicProperties
 
 test_cases = [TestDynamicProgrammingBasicProperties,
-              TestEnergyMinimizationBasicProperties,
               TestPerceptronBasicProperties]
 
