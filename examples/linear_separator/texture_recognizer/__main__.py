a7e4f811b1910d4da4ca075414bb87c4dd154631
char-lie <webmaster976@gmail.com>
2016-04-24 20:21:51 +0300

    Improved LSS Texture Recognizer example
    Learning from given image by mask, not by manual setting of example neighbours.
    Testing result on bigger image.

--- examples/linear_separator/texture_recognizer/__main__.py
+++ examples/linear_separator/texture_recognizer/__main__.py
@@ -1,52 +1,52 @@
 from Texture import Texture
 from os.path import join as pjoin, dirname
 
-
 from PIL import Image
 
 
 if __name__ == '__main__':
     texture = Texture(2, 2, 2)
     image = Image.open(pjoin(dirname(__file__), 'stripes.png')).convert('L')
+    mask = Image.open(pjoin(dirname(__file__), 'mask.png'))
     width, height = image.size
 
-    texture.pick_texture_sample({
-        0: (0, 1),
-        1: (0, 0)
-    }, 0)
-    texture.pick_texture_sample({
-        0: (1, 0),
-        1: (1, 1)
-    }, 0)
-    texture.pick_texture_sample({
-        0: (0, 0),
-        1: (0, 0)
-    }, 0)
-
-    texture.pick_texture_sample({
-        1: (0, 1),
-        0: (0, 0)
-    }, 1)
-    texture.pick_texture_sample({
-        1: (1, 0),
-        0: (1, 1)
-    }, 1)
-
     data = list(image.getdata())
-    results = []
-    for x in xrange(width):
-        row = []
-        for y in xrange(height):
+    mask_data = list(mask.getdata())
+    for x in xrange(height-1):
+        for y in xrange(width-1):
             cur = data[x*width+y]
             cur = 0 if cur == 0 else 1
 
             params = {}
-            if y < width-1:
-                r = data[x*width+(y+1)]
+
+            r = data[x*width+(y+1)]
+            r = 0 if r == 0 else 1
+            params[0] = (cur, r)
+
+            b = data[(x+1)*width+y]
+            b = 0 if b == 0 else 1
+            params[1] = (cur, b)
+
+            current_class = 0 if mask_data[x*width+y][0] == 255 else 1
+            texture.pick_texture_sample(params, current_class)
+
+    test_image = Image.open(pjoin(dirname(__file__), 'test.png')).convert('L')
+    test_width, test_height = test_image.size
+    test_data = list(test_image.getdata())
+    results = []
+    for x in xrange(test_height):
+        row = []
+        for y in xrange(test_width):
+            cur = test_data[x*test_width+y]
+            cur = 0 if cur == 0 else 1
+
+            params = {}
+            if y < test_width-1:
+                r = test_data[x*test_width+(y+1)]
                 r = 0 if r == 0 else 1
                 params[0] = (cur, r)
-            if x < height-1:
-                b = data[(x+1)*width+y]
+            if x < test_height-1:
+                b = test_data[(x+1)*test_width+y]
                 b = 0 if b == 0 else 1
                 params[1] = (cur, b)
 
@@ -55,9 +55,7 @@ if __name__ == '__main__':
         results += row
 
     results = [((200, 50, 50) if r == 0 else (50, 50, 200)) for r in results]
-    result = Image.new('RGB', image.size)
+    result = Image.new('RGB', test_image.size)
     result.putdata(results)
-    for x in xrange(width):
-        print results[x*height:(x+1)*height]
     result.save(pjoin(dirname(__file__), 'result.png'))
 
