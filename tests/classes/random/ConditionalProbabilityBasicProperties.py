c6c9f214745e23f2745b451f5b80ad491757f2f0
char-lie <webmaster976@gmail.com>
2016-04-29 23:38:53 +0300

    Updated Conditional Probability unit tests
    Tested method for adding samples

--- tests/classes/random/ConditionalProbabilityBasicProperties.py
+++ tests/classes/random/ConditionalProbabilityBasicProperties.py
@@ -7,7 +7,13 @@ class ConditionalProbabilityBasicProperties(TestCase):
 
 
     def setUp(self):
-        pass
+        def as_tuples(x):
+            return [tuple(row) for row in x]
+        self.processors = [
+            lambda x: x,
+            as_tuples,
+            lambda x: set(as_tuples(x))
+        ]
 
 
     def tearDown(self):
@@ -17,6 +23,22 @@ class ConditionalProbabilityBasicProperties(TestCase):
     def test_constructor(self):
         p = ConditionalProbability()
         self.assertIsInstance(p, ConditionalProbability)
+
+
+    def test_add_sample(self):
+        sample = [[0, 1], [1, 0]]
+        for process in self.processors:
+            p = ConditionalProbability()
+            p.add_sample(process(sample))
+
+
+    def test_add_samples(self):
+        sampleA = [[0, 1], [1, 0]]
+        sampleB = [[2, 3], [3, 2]]
+        for process in self.processors:
+            p = ConditionalProbability()
+            p.add_sample(process(sampleA))
+            p.add_sample(process(sampleB))
 
 
 if __name__ == '__main__':
