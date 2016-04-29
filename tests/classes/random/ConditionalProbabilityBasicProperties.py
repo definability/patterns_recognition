cfebd24f63e7878e7de8f1e701c476a2a4c0a338
char-lie <webmaster976@gmail.com>
2016-04-29 21:37:26 +0300

    Initiated Conditional Probability unit tests

---
+++ tests/classes/random/ConditionalProbabilityBasicProperties.py
@@ -0,0 +1,19 @@
+from unittest import TestCase, main
+
+from classes.random import ConditionalProbability
+
+
+class ConditionalProbabilityBasicProperties(TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    main()
+
--- tests/classes/random/__init__.py
+++ tests/classes/random/__init__.py
@@ -1,4 +1,6 @@
 from GibbsSamplerBasicProperties import GibbsSamplerBasicProperties
+from ConditionalProbabilityBasicProperties import ConditionalProbabilityBasicProperties
 
-test_cases = [GibbsSamplerBasicProperties]
+test_cases = [GibbsSamplerBasicProperties,
+              ConditionalProbabilityBasicProperties]
 
