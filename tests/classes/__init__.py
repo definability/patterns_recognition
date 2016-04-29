a4fff6e828f61656d2f77213874c1b710b4375f5
char-lie <5962274+char-lie@users.noreply.github.com>
2016-04-29 19:53:15 +0300

    Initiated Gibbs Sampler basi unit tests

--- tests/classes/__init__.py
+++ tests/classes/__init__.py
@@ -2,7 +2,8 @@ import semiring
 import graph
 import solver
 import image
+import random
 
 test_cases = sum([semiring.test_cases, graph.test_cases, solver.test_cases,
-                  image.test_cases], [])
+                  image.test_cases, random.test_cases], [])
 
---
+++ tests/classes/random/__init__.py
@@ -0,0 +1,4 @@
+from GibbsSamplerBasicProperties import GibbsSamplerBasicProperties
+
+test_cases = [GibbsSamplerBasicProperties]
+
