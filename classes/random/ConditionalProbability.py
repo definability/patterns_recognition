2d0b06b738c0e040b7b5653730dde4f8f7670e3d
char-lie <5962274+char-lie@users.noreply.github.com>
2016-04-29 23:39:55 +0300

    Added probabilities getter for CP
    Getter estimates probability by given sample.
    Nevertheless, it can be tractated not as an estimate, but a real
    probability, if we want to calculate probability for a vector element to
    have specific value, if we know, that the vector will be taken from the
    samples.
    
    Input:
    - index of unknown element;
    - values of other elements in their order in vector.
    Output:
    - blank dictionary if such values of surrounding vectors don't exist in
      samples;
    - dictionary with values and their probabilities:
    ```
    {
        V1: p1,
        V2: P2,
        ...
        Vn: Pn
    }
    ```

--- classes/random/ConditionalProbability.py
+++ classes/random/ConditionalProbability.py
@@ -1,4 +1,5 @@
-from numpy import array, concatenate
+from numpy import array, concatenate, unique, histogram, zeros
+from itertools import izip
 
 
 class ConditionalProbability:
@@ -16,4 +17,22 @@ class ConditionalProbability:
             self.__sample = array(sample)
         else:
             self.__sample = concatenate((self.__sample, sample))
+
+
+    def get_probabilities(self, i, values):
+        indices = (self.__sample == values)
+        lefts = indices[:,:i].all(axis=1)
+        rights = indices[:,i+1:].all(axis=1)
+        indices = lefts & rights
+
+        samples = self.__sample[lefts & rights]
+        occurences = samples[:,i]
+        if occurences.size == 0:
+            return {}
+
+        values = unique(occurences)
+        values.sort()
+        hist = histogram(occurences, concatenate((values, [values[-1]])))[0]
+
+        return dict(izip(values, hist/hist.sum().astype('float')))
 
