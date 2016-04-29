7b46ecd2a4d262b385995c0373e9a573fd6e4c2e
char-lie <webmaster976@gmail.com>
2016-04-29 23:38:22 +0300

    Updated Conditional Probability
    Added method to add samples to estimate by

--- classes/random/ConditionalProbability.py
+++ classes/random/ConditionalProbability.py
@@ -1,6 +1,19 @@
+from numpy import array, concatenate
+
+
 class ConditionalProbability:
 
 
     def __init__(self):
         self.__sample = None
+
+
+    def add_sample(self, sample):
+        if type(sample) is set:
+            sample = list(sample)
+
+        if self.__sample is None:
+            self.__sample = array(sample)
+        else:
+            self.__sample = concatenate((self.__sample, sample))
 
