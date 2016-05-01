c6fbd5df0b7a162f7ad1f3c31ecaf0b07633474b
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-01 18:44:12 +0300

    Fixed conda installation

--- install-conda.sh
+++ install-conda.sh
@@ -1,9 +1,7 @@
 wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
+export PATH="/homt/travis/miniconda/bin:$PATH"
 #hash -r
 #conda config --set always_yes yes --set changeps1 no
 conda update --yes conda
-
-conda install --yes python=$TRAVIS_PYTHON_VERSION numpy scipy
 
