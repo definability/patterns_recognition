4163f42ee09f7eca1abdcd21cf1350ced0e739b5
char-lie <webmaster976@gmail.com>
2016-05-01 18:51:55 +0300

    Fixed conda installation

--- install-conda.sh
+++
@@ -1,7 +0,0 @@
-wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p /home/travis/miniconda
-export PATH="/home/travis/miniconda/bin:$PATH"
-#hash -r
-#conda config --set always_yes yes --set changeps1 no
-conda update --yes conda
-
