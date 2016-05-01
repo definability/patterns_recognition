ac04b2201275d584738778f1c86cb7d720c1af02
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-01 16:00:02 +0300

    Updated Conditional Probability class
    Updated get probabilities method:
    - allowed to exclude vector components (averaging by them);

--- classes/random/ConditionalProbability.py
+++ classes/random/ConditionalProbability.py
@@ -1,4 +1,4 @@
-from numpy import array, concatenate, unique, histogram, zeros
+from numpy import array, concatenate, unique, histogram, full
 from itertools import izip
 
 
@@ -19,20 +19,27 @@ class ConditionalProbability:
             self.__sample = concatenate((self.__sample, sample))
 
 
-    def get_probabilities(self, i, values):
-        indices = (self.__sample == values)
-        lefts = indices[:,:i].all(axis=1)
-        rights = indices[:,i+1:].all(axis=1)
-        indices = lefts & rights
+    def get_probabilities(self, i, values=None, exclude=None):
+        if exclude is None:
+            exclude = [i]
+        else:
+            exclude.append([i])
 
-        samples = self.__sample[lefts & rights]
+        if values is not None:
+            mask = full(self.__sample.shape[1], True, dtype=bool)
+            mask[exclude] = False
+            indices = (self.__sample[:,mask] == values).all(axis=1)
+            samples = self.__sample[indices]
+        else:
+            samples = self.__sample
+
         occurences = samples[:,i]
         if occurences.size == 0:
             return {}
 
         values = unique(occurences)
         values.sort()
-        hist = histogram(occurences, concatenate((values, [values[-1]])))[0]
+        hist, _ = histogram(occurences, concatenate((values, [values[-1]])))
 
         return dict(izip(values, hist/hist.sum().astype('float')))
 
