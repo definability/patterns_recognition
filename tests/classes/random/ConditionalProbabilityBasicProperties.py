0c45061e7f8f19b3006ee5eca64e78e80ea22719
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-01 17:04:34 +0300

    Updated CP unit test

--- tests/classes/random/ConditionalProbabilityBasicProperties.py
+++ tests/classes/random/ConditionalProbabilityBasicProperties.py
@@ -12,7 +12,6 @@ class ConditionalProbabilityBasicProperties(TestCase):
         self.processors = [
             lambda x: x,
             as_tuples,
-            lambda x: set(as_tuples(x))
         ]
 
 
@@ -52,6 +51,39 @@ class ConditionalProbabilityBasicProperties(TestCase):
 
             self.assertAlmostEqual(p.get_probabilities(1, [0])[0], .5)
             self.assertAlmostEqual(p.get_probabilities(1, [0])[1], .5)
+
+
+    def test_get_probabilities_one_dimensional(self):
+        sample = [[0], [1], [1], [1]]
+        for process in self.processors:
+            p = ConditionalProbability()
+            p.add_sample(process(sample))
+            self.assertAlmostEqual(p.get_probabilities(0)[0], .25)
+            self.assertAlmostEqual(p.get_probabilities(0)[1], .75)
+
+
+    def test_get_probabilities_excluding(self):
+        sample = [[0, 0], [0, 1]]
+        for process in self.processors:
+            p = ConditionalProbability()
+            p.add_sample(process(sample))
+
+            self.assertAlmostEqual(p.get_probabilities(0, exclude=[1])[0], 1.)
+
+            self.assertAlmostEqual(p.get_probabilities(1, exclude=[1])[0], .5)
+            self.assertAlmostEqual(p.get_probabilities(1, exclude=[1])[1], .5)
+
+
+    def test_get_probabilities_middle(self):
+        sample = [[0, 0, 0], [1, 0, 1], [1, 1, 1], [1, 0, 1]]
+        for process in self.processors:
+            p = ConditionalProbability()
+            p.add_sample(process(sample))
+
+            self.assertAlmostEqual(p.get_probabilities(1, [0, 0])[0], 1.)
+
+            self.assertAlmostEqual(p.get_probabilities(1, [1, 1])[0], 2./3)
+            self.assertAlmostEqual(p.get_probabilities(1, [1, 1])[1], 1./3)
 
 
     def test_unknown_values(self):
